e207b476b06bb3e3fa46553fc5edfb00273ec578
AppleMint_Git <llyyyy7@gmail.com>
2014-06-05 17:03:27 +0900

    text저장한 것을 load하기

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+<meta charset="utf-8">
+
 function autoSave(event){
 	//contents 내용 가져오기
 	var value = $('#contents').val();
@@ -5,5 +7,13 @@ function autoSave(event){
 	// console.log(value);
 }
 
+function autoLoad(event){
+	$('#contents').val(window.localStorage.getItem('note_contents'));
+}
+
+
 // function()으로 익명함수주는 것 보다 이게 낫다.
 $('$contents').on('keyup',autoSave);
+
+// 저장된 text 불러오기
+$(window).on('load',autoLoad);
